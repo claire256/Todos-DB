ad28e8e6af1f2a53f8e0881b8a1673da1184ead2
claire256 <clairenabwami3@gmail.com>
2022-07-26 20:30:47 +0300

    Add if statements

--- controllers.js
+++ controllers.js
@@ -46,15 +46,103 @@ const addTodo = (req,res) =>{
       })               
 }
 
-// 
 const getAllTodos = (req, res)=>{
     db.query(`select * from todos`, (err, result)=>{
-        res.status(200).send({
+      if(err) {
+        return  res.status(400).send({
+            status: 'fail',
+            data: err.message
+        })
+    }
+
+    else {
+        return res.status(200).send({
+           status: 'successful',
             data: result.rows
         })
+    }
+    })
+
+}
+
+const getTodo = (req,res)=> {
+    
+const values = [req.params.id]
+
+    db.query(`select * from todos where id = $1`, values, (err, result)=>{
+        if(err) {
+            return  res.status(400).send({
+                status: 'fail',
+                data: err.message
+            })
+        }
+    
+        else {
+            return res.status(200).send({
+               status: 'successful',
+                data: result.rows
+            })
+        }
+        
     })
 }
+
+const editTodo = (req, res)=> {
+    const todo = req.body;
+                     
+     const values = [todo.title, todo.description, todo.date]
+
+    const editQuery = `update todos set
+     title = $1, 
+     description = $2, 
+     date = $3
+     where id = '${req.params.id}' `
+
+      db.query(editQuery, values, (err, result)=> {
+          if(err){
+        return res.status(400).send({
+             status: 'edit failed',
+             data: err.message
+          })
+         }
+
+         else{
+             return res.status(200).send({
+                 status: 'edit successful'
+             })
+         }
+        })
+    }
+
+const deleteTodo = (req, res) => {
+     
+    const values = [req.params.id]
+
+    db.query(`delete from todos where id = $1`, values, (err, result)=> {
+      
+    if(err) {
+    return  res.status(400).send({
+        status: 'fail',
+        data: err.message
+    })
+   }
+
+   else {
+    return res.status(200).send({
+       status: 'successful',
+        data: result.rows
+    })
+   }
+
+  })
+
+}
+    
+
 module.exports = {
     addTodo,
-    getAllTodos
+    getAllTodos,
+    getTodo,
+    editTodo,
+    deleteTodo
 }
--- routes.js
+++ routes.js
@@ -1,8 +1,12 @@
 const Todo = require('./controllers');
 const db = require('./database');
 const router = (app) => {
+    
     app.post('/todos', Todo.addTodo);
     app.get('/todos', Todo.getAllTodos);
+    app.get('/todos/:id', Todo.getTodo);
+    app.put('/todos/:id', Todo.editTodo )
+    app.delete('/todos/:id', Todo.deleteTodo)
     
 }
 
