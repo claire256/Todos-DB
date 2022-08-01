eea2716b6c313007e08a1d206896bb1d368a1742
claire256 <clairenabwami3@gmail.com>
2022-08-01 16:38:02 +0300

    Add user table

--- tables.js
+++ tables.js
@@ -1,14 +1,23 @@
  const db = require('./database');
+ const usersTableQuery = `CREATE TABLE IF NOT EXISTS users(
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  first_name VARCHAR(8) NOT NULL,
+  last_name VARCHAR(8) NOT NULL,
+  email VARCHAR(15) NOT NULL,
+  password VARCHAR(15) NOT NULL)`
 
  const todosTableQuery = `CREATE TABLE IF NOT EXISTS todos (
      id BIGSERIAL NOT NULL PRIMARY KEY, 
      title VARCHAR(10) NOT NULL, 
      description VARCHAR(15),
-     date DATE NOT NULL)`
-  async function createTables(){
+     date DATE NOT NULL,
+     users_id BIGINT REFERENCES users(id))`
+  
+     async function createTables(){
 
  try {
-   const res = await db.query(todosTableQuery)  
+    await db.query(usersTableQuery)  
+    await db.query(todosTableQuery)
    console.log('table created')  
   } catch (error) {
    console.log('error occurred', error)  
